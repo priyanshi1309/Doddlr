7e6958617fa322eaae312864c8acc94c9d42b747
Priyanshi Pandey <palakmanu1309@gmail.com>
2024-10-07 20:14:37 +0530

    fourt commit

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -17,10 +17,10 @@ const CameraRig = ({ children }) => {
     let targetPosition = [-0.4, 0, 2];
     if(snap.intro) {
       if(isBreakpoint) targetPosition = [0, 0, 2];
-      if(isMobile) targetPosition = [0, 0.2, 2.5];
+      if(isMobile) targetPosition = [0, -0.2, 2.5];
     } else {
       if(isMobile) targetPosition = [0, 0, 2.5]
-      else targetPosition = [0, 0, 2];
+      else targetPosition = [0, -1.0, 2];
     }
 
     // set model camera position
